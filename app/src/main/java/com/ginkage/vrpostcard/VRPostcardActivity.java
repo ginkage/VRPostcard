7aae84d56671d478a8a58e742d99a8f4c62f1aac
Ivan Podogov <ginkage@yandex.ru>
2014-11-09 00:50:54 +0400

    Styling fixes

--- app/src/main/java/com/ginkage/vrpostcard/VRPostcardActivity.java
+++ app/src/main/java/com/ginkage/vrpostcard/VRPostcardActivity.java
@@ -131,73 +131,6 @@ public class VRPostcardActivity extends CardboardActivity implements CardboardVi
 
 	private Vibrator mVibrator;
 
-	/**
-	 * Converts a raw text file into a string.
-	 * @param resId The resource ID of the raw text file about to be turned into a shader.
-	 * @return
-	 */
-	private String readRawTextFile(int resId)
-	{
-		InputStream inputStream = getResources().openRawResource(resId);
-		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-			StringBuilder sb = new StringBuilder();
-			String line;
-			while ((line = reader.readLine()) != null) {
-				sb.append(line).append("\n");
-			}
-			reader.close();
-			return sb.toString();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
-
-	/**
-	 * Converts a raw text file, saved as a resource, into an OpenGL ES shader
-	 * @param type The type of shader we will be creating.
-	 * @param resId The resource ID of the raw text file about to be turned into a shader.
-	 * @return
-	 */
-	private int loadGLShader(int type, int resId)
-	{
-		String code = readRawTextFile(resId);
-		int shader = GLES20.glCreateShader(type);
-		GLES20.glShaderSource(shader, code);
-		GLES20.glCompileShader(shader);
-
-		// Get the compilation status.
-		final int[] compileStatus = new int[1];
-		GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
-
-		// If the compilation failed, delete the shader.
-		if (compileStatus[0] == 0) {
-			Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shader));
-			GLES20.glDeleteShader(shader);
-			shader = 0;
-		}
-
-		if (shader == 0) {
-			throw new RuntimeException("Error creating shader.");
-		}
-
-		return shader;
-	}
-
-	/**
-	 * Checks if we've had an error inside of OpenGL ES, and if so what that error is.
-	 * @param func
-	 */
-	private static void checkGLError(String func)
-	{
-		int error;
-		while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
-			Log.e(TAG, func + ": glError " + error);
-			throw new RuntimeException(func + ": glError " + error);
-		}
-	}
-
 	private int createBuffer(float[] buffer)
 	{
 		FloatBuffer floatBuf = ByteBuffer.allocateDirect(buffer.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
@@ -374,6 +307,73 @@ public class VRPostcardActivity extends CardboardActivity implements CardboardVi
 		handles[1] = renderTex;
 
 		return res;
+	}
+
+	/**
+	 * Converts a raw text file into a string.
+	 * @param resId The resource ID of the raw text file about to be turned into a shader.
+	 * @return
+	 */
+	private String readRawTextFile(int resId)
+	{
+		InputStream inputStream = getResources().openRawResource(resId);
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line).append("\n");
+			}
+			reader.close();
+			return sb.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	/**
+	 * Converts a raw text file, saved as a resource, into an OpenGL ES shader
+	 * @param type The type of shader we will be creating.
+	 * @param resId The resource ID of the raw text file about to be turned into a shader.
+	 * @return
+	 */
+	private int loadGLShader(int type, int resId)
+	{
+		String code = readRawTextFile(resId);
+		int shader = GLES20.glCreateShader(type);
+		GLES20.glShaderSource(shader, code);
+		GLES20.glCompileShader(shader);
+
+		// Get the compilation status.
+		final int[] compileStatus = new int[1];
+		GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+		// If the compilation failed, delete the shader.
+		if (compileStatus[0] == 0) {
+			Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shader));
+			GLES20.glDeleteShader(shader);
+			shader = 0;
+		}
+
+		if (shader == 0) {
+			throw new RuntimeException("Error creating shader.");
+		}
+
+		return shader;
+	}
+
+	/**
+	 * Checks if we've had an error inside of OpenGL ES, and if so what that error is.
+	 * @param func
+	 */
+	private static void checkGLError(String func)
+	{
+		int error;
+		while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+			Log.e(TAG, func + ": glError " + error);
+			throw new RuntimeException(func + ": glError " + error);
+		}
 	}
 
 	private int Compile(int vsId, int fsId)
